026b0fa136b557c161bf541838d54b8c0ab6dc6a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-04-09 05:26:40 +0300

    Helper utilities to execute test code on particular platforms

--- libraries/stdlib/common/test/testUtils.kt
+++ libraries/stdlib/common/test/testUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,3 +8,6 @@ package test
 public expect fun assertTypeEquals(expected: Any?, actual: Any?)
 
 internal expect fun String.removeLeadingPlusOnJava6(): String
+
+public expect fun testOnJvm(action: () -> Unit)
+public expect fun testOnJs(action: () -> Unit)
