54b95a82609b7c83291e1bf0bf4565df639cf85e
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2019-04-11 16:25:40 +0300

    [JS IR BE] Simplify IR runtime sourceset

--- libraries/tools/idl2k/src/main/kotlin/main.kt
+++ libraries/tools/idl2k/src/main/kotlin/main.kt
@@ -6,10 +6,10 @@ import java.io.*
 fun main(args: Array<String>) {
     val webIdl = BuildWebIdl(
             mdnCacheFile = File("target/mdn-cache.txt"),
-            srcDir = File("../../stdlib/js/idl"))
+            srcDir = File("../../stdlib/js-common/idl"))
 
     println("Generating...")
 
     val copyrightNotice = readCopyrightNoticeFromProfile(File("../../../.idea/copyright/apache.xml"))
-    webIdl.jsGenerator(File("../../stdlib/js/src/org.w3c"), copyrightNotice)
+    webIdl.jsGenerator(File("../../stdlib/js-common/src/org.w3c"), copyrightNotice)
 }
