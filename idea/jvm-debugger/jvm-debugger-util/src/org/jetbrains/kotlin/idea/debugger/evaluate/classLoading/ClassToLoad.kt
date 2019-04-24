ae1decb797c9ae9baff554a26f999ab7e89a60b3
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2019-04-24 21:44:36 +0300

    ++ Move out JVM debugger functionality

--- idea/jvm-debugger/jvm-debugger-util/src/org/jetbrains/kotlin/idea/debugger/evaluate/classLoading/ClassToLoad.kt
+++ idea/jvm-debugger/jvm-debugger-util/src/org/jetbrains/kotlin/idea/debugger/evaluate/classLoading/ClassToLoad.kt
@@ -16,7 +16,8 @@
 
 package org.jetbrains.kotlin.idea.debugger.evaluate.classLoading
 
-import org.jetbrains.kotlin.idea.debugger.evaluate.GENERATED_CLASS_NAME
+const val GENERATED_FUNCTION_NAME = "generated_for_debugger_fun"
+const val GENERATED_CLASS_NAME = "Generated_for_debugger_class"
 
 @Suppress("ArrayInDataClass")
 data class ClassToLoad(val className: String, val relativeFileName: String, val bytes: ByteArray) {
