5e0f5675159e08bddfa0a4abe6ffd460c3cd8483
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2019-04-26 07:38:57 +0300

    Few migration

--- idea/testData/inspectionsLocal/convertPairConstructorToToFunction/invalidArgs.kt
+++ idea/testData/inspectionsLocal/convertPairConstructorToToFunction/invalidArgs.kt
@@ -1,7 +1,7 @@
 // PROBLEM: none
 // WITH_RUNTIME
 // ERROR: No value passed for parameter 'second'
-// ERROR: Type inference failed: Not enough information to infer parameter B in constructor Pair<out A, out B>(first: A, second: B)<br>Please specify it explicitly.<br>
+// ERROR: Not enough information to infer type variable B
 import kotlin.Pair
 fun test() {
     val p = <caret>Pair(1, )
